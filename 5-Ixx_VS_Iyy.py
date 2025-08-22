71d746b1565861815812cae1bc8ec05aba934f67
puriishaan <125255998+puriishaan@users.noreply.github.com>
2025-08-22 14:30:09 +0800

    Update 5-Ixx_VS_Iyy.py

--- 5-Ixx_VS_Iyy.py
+++ 5-Ixx_VS_Iyy.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 # Load model and scaler
 # -------------------------------
 
-# ✅ Load without compile to avoid the 'mse' error
+
 model = keras.models.load_model("my_10d_nn_model1.h5", compile=False)
 scaler = joblib.load("scaler1.joblib")
 
